f15045cc33d66b48678d6e472b9fa69a1db301b2
ken-arthur005 <careyckenarthur@gmail.com>
2025-10-31 13:54:13 +0000

    more additionsss

--- voting_system/src/app/admin/dashboard/page.jsx
+++ voting_system/src/app/admin/dashboard/page.jsx
@@ -1,27 +1,66 @@
 "use client"
 
-import {
-    Sidebar,
-    SidebarProvider,
-} from "@/components/ui/sidebar"
+import { MdOutlineAnalytics } from "react-icons/md";
+import { HiHandThumbUp } from "react-icons/hi2";
+import { IoIosPeople } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
 
+import Image from "next/image"
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: "300",
+  subsets: ["latin"],
+});
 
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-[20vw_80vw] grid-rows-[20vw_80vw]">
-        <SidebarProvider>
-            <Sidebar className="col-span-2 w-[20vw] rounded-2xl my-3 ml-3">
-                
-                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                      
-                    </header>
-                    <div className="flex flex-1 flex-col gap-4 p-4">
+    <div className="grid grid-cols-[15vw_85vw] grid-rows-[20vh_80vh]">
+            {/* Sidebar */}
+            <div className="row-span-2 rounded-2xl mx-4 my-4 border-1 bg-gray-100 grid grid-rows-[20%_40%_40%] ">
+              {/* logo */}
+              <div className="p-5 ml-3">
+                <Image
+                  className=""
+                  src="/owass_crest.png" 
+                  alt="owass"
+                  width={100}
+                  height={100}
+                />
+              </div>
+
+              {/* content */}
+              <div className={`p-5 flex-col ${montserrat.className} antialiased ml-2 my-auto justify-center items-end`}>
+                <h1 className="font-semibold my-5">Menu</h1>
+                <div className="flex my-5"><MdOutlineAnalytics size={20} className="mr-2"/><h2>Analytics</h2></div>
+                <div className="flex my-5"><HiHandThumbUp size={20} className="mr-2 "/><h2>Elections</h2></div>
+                <div className="flex my-5"><IoIosPeople  size={20} className="mr-2 "/><h2>Candidate and Portfolio Management</h2></div>
+              </div>
+
+              {/* footer */}
+              <div className={`p-5 flex-col ${montserrat.className} antialiased ml-2 my-auto items-end`}>
+                <h1 className="font-semibold my-5">General</h1>
+                <div className="flex my-5"><IoSettingsOutline size={20} className="mr-2"/><h2>Settings</h2></div>
+                <div className="flex my-5"><IoIosLogOut size={20} className="mr-2 "/><h2>Logout</h2></div>
+              </div>
                     
-                    
-                    </div>
                 
-            </Sidebar>
-        </SidebarProvider>
+            </div>
+
+            {/* NAVBAR */}
+            <div className="rounded-2xl  border-2 bg-gray-200 mr-4 mb-2 mt-4">
+                
+                   
+            </div>
+
+            {/* THE MAIN SECTION */}
+            <div className="rounded-2xl border-2 bg-gray-200 mr-4 mb-4 mt-2">
+                 
+                   
+            </div>
+    
+    
     </div>
     
   )
