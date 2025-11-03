eb142dba113f79b422630aa3fae8ac178f76dc5c
ken-arthur005 <careyckenarthur@gmail.com>
2025-11-03 16:58:11 +0000

    more additionssss

--- voting_system/src/app/admin/dashboard/page.jsx
+++ voting_system/src/app/admin/dashboard/page.jsx
@@ -5,6 +5,9 @@ import { HiHandThumbUp } from "react-icons/hi2";
 import { IoIosPeople } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoIosLogOut } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { IoIosNotificationsOutline } from "react-icons/io";
 
 import Image from "next/image"
 import { Montserrat } from "next/font/google";
@@ -16,23 +19,26 @@ const montserrat = Montserrat({
 
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-[15vw_85vw] grid-rows-[20vh_80vh]">
+    <div className="grid grid-cols-[15vw_85vw] grid-rows-[10vh_90vh]">
             {/* Sidebar */}
-            <div className="row-span-2 rounded-2xl mx-4 my-4 border-1 bg-gray-100 grid grid-rows-[20%_40%_40%] ">
+            <div className="row-span-2 rounded-2xl mx-4 my-4 border-1 bg-gray-100 grid grid-rows-[15%_45%_40%] ">
               {/* logo */}
-              <div className="p-5 ml-3">
+              <div className={`p-5  flex ${montserrat.className} antialiased`}>
                 <Image
-                  className=""
+                  className="h-[50px]"
                   src="/owass_crest.png" 
                   alt="owass"
-                  width={100}
-                  height={100}
+                  width={50}
+                  height={50}
                 />
+                <h2 className={` mx-2.5  font-semibold`}>
+                  Akatakyie E-voting platform
+                </h2>
               </div>
 
               {/* content */}
-              <div className={`p-5 flex-col ${montserrat.className} antialiased ml-2 my-auto justify-center items-end`}>
-                <h1 className="font-semibold my-5">Menu</h1>
+              <div className={`flex-col ${montserrat.className} antialiased ml-6 justify-center items-end`}>
+                <h1 className=" my-5 text-sm">Menu</h1>
                 <div className="flex my-5"><MdOutlineAnalytics size={20} className="mr-2"/><h2>Analytics</h2></div>
                 <div className="flex my-5"><HiHandThumbUp size={20} className="mr-2 "/><h2>Elections</h2></div>
                 <div className="flex my-5"><IoIosPeople  size={20} className="mr-2 "/><h2>Candidate and Portfolio Management</h2></div>
@@ -40,7 +46,7 @@ const Dashboard = () => {
 
               {/* footer */}
               <div className={`p-5 flex-col ${montserrat.className} antialiased ml-2 my-auto items-end`}>
-                <h1 className="font-semibold my-5">General</h1>
+                <h1 className="my-5 text-sm">General</h1>
                 <div className="flex my-5"><IoSettingsOutline size={20} className="mr-2"/><h2>Settings</h2></div>
                 <div className="flex my-5"><IoIosLogOut size={20} className="mr-2 "/><h2>Logout</h2></div>
               </div>
@@ -49,9 +55,36 @@ const Dashboard = () => {
             </div>
 
             {/* NAVBAR */}
-            <div className="rounded-2xl  border-2 bg-gray-200 mr-4 mb-2 mt-4">
+            <div className="rounded-2xl  border-2 bg-gray-200 mr-4 mb-2 mt-4  items-center flex justify-between">
+                {/*Search bar */}
+                <div className=" rounded-full bg-white ml-5 w-[20%] h-[50%] flex items-center">
+                  <CiSearch size={20} className="mx-4"/>
+                  <input type="text" id="name" size={25} className={`h-[60%] ${montserrat.className}`} />
+                </div>
+
                 
-                   
+              {/*USER */}
+
+              <div className="flex mr-5 items-center">
+
+
+                <div className="">
+                  <IoIosNotificationsOutline size={25} className="mx-2" />
+                </div>
+                <div className="rounded-full p-1 bg-amber-300 mx-2">
+                  <Avatar>
+                    <AvatarImage src="/messi.png" />
+                    <AvatarFallback>LM</AvatarFallback>
+                  </Avatar>
+                </div>
+    
+                <div className={`${montserrat.className} mr-2`}>
+                  <h2 className="text-xs font-semibold">Lionel Messi</h2>
+                  <h3 className="text-xs">Admin</h3>
+                </div>
+              </div>
+              
+
             </div>
 
             {/* THE MAIN SECTION */}
