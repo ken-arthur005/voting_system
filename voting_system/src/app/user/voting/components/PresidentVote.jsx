773f8554e6ae1b5ddab6fa820a6388f488eab9a8
ken-arthur005 <careyckenarthur@gmail.com>
2025-11-26 00:54:55 +0000

    more shit

--- voting_system/src/app/user/voting/components/PresidentVote.jsx
+++ voting_system/src/app/user/voting/components/PresidentVote.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { President } from './presidents'
+import Image from 'next/image'
 const PresidentVote = () => {
   return (
     <div className='min-h-[95vh] max-w-[90vw]'>
-        <h1 className='text-center'> President </h1>
+        <h1 className='text-center text-3xl'> President </h1>
+        <h2 className='text-center text-2xl'>Vote for your prefered candidate</h2>
+        
+        <div className='flex justify-center gap-10 my-10'>
+            {President.map((i)=>{
+              const Name = i.name
+              const picture = i.image
+              return(
+                <div key={i.id}>
+                  <Image src={picture}  
+                  width={200} 
+                  height={200}
+                  className="grayscale brightness-75 hover:grayscale-0 hover:brightness-100 transition-all duration-300"
+                  />
+                  <h2 className='text-2xl '>{Name}</h2>
+                </div>
+              )
+            })}
+        </div>
         
     </div>
   )
--- voting_system/src/app/user/voting/components/presidents.js
+++ voting_system/src/app/user/voting/components/presidents.js
@@ -1,4 +1,22 @@
-const President = [{
-    name: "Kelvin Owusu",
-    
-}]
+export const President = [
+    {
+    id:4,
+    name: "Gojo Satoru",
+    image: "/gojo.jpg",
+    },
+    {
+    id:3,
+    name: "Jinwoo",
+    image: "/jinwoo.jpg"
+    },
+    {
+    id: 2,
+    name: "levi Ackerman",
+    image: "/levi.jpg"
+    },
+    {
+      id: 1,
+      name: "John Wick",
+      image: "/wick.jpg"  
+    }
+    ]
